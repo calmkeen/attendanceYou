abebfda48e1047bc3eddd0b3d589b705fe09df0d
Younsung <78361650+calmkeen@users.noreply.github.com>
2025-08-03 15:36:34 +0900

    FEAT: Add top navigationBar button
    Add list,alram & observe tab state for alram,back button

--- attandanceApp/Common/Presentation/NavigationBar.swift
+++ attandanceApp/Common/Presentation/NavigationBar.swift
@@ -14,8 +14,9 @@ class NavigationBar: UINavigationBar {
     private let disposeBag = DisposeBag()
     
     var navigationTitle = UILabel()
-    var navigationButton = UIButton()
-    
+    var naviBackButton = UIButton()
+    var naviAlertBtn = UIButton()
+    var naviListBtn = UIButton()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -36,12 +37,19 @@ class NavigationBar: UINavigationBar {
         navigationTitle.textColor = .black
         navigationTitle.textAlignment = .center
         
-        navigationButton.setImage(UIImage(systemName: "chevron.left"), for: .normal)
-        navigationButton.tintColor = .black
-        navigationButton.isHidden = true // 기본적으로 숨김
+        naviBackButton.setImage(UIImage(systemName: "chevron.left"), for: .normal)
+        naviBackButton.tintColor = .black
+        naviBackButton.isHidden = true // 기본적으로 숨김
+        
+        naviListBtn.setImage(UIImage(systemName: "list.dash"), for: .normal)
+        
+        naviAlertBtn.setImage(UIImage(systemName: "bell"), for: .normal)
+        
         
         addSubview(navigationTitle)
-        addSubview(navigationButton)
+        addSubview(naviBackButton)
+        addSubview(naviListBtn)
+        addSubview(naviAlertBtn)
         
         setupConstraints()
         bindTabState()
@@ -70,19 +78,34 @@ class NavigationBar: UINavigationBar {
             make.centerX.equalToSuperview()
         }
         
-        navigationButton.snp.makeConstraints { make in
+        naviBackButton.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().offset(16)
             make.leading.equalToSuperview().offset(16)
             make.centerY.equalToSuperview()
             make.width.height.equalTo(24)
         }
+        
+        naviAlertBtn.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().offset(16)
+            make.leading.equalToSuperview().offset(16)
+            make.centerY.equalToSuperview()
+            make.width.height.equalTo(24)
+        }
+        naviListBtn.snp.makeConstraints{ make in
+            make.bottom.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+            make.centerY.equalToSuperview()
+            make.width.height.equalTo(24)
+        }
+        
     }
     
     func showBackButton(_ show: Bool) {
-        navigationButton.isHidden = !show
+        naviBackButton.isHidden = !show
     }
     
     func setBackButtonAction(_ action: @escaping () -> Void) {
-        navigationButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+        naviBackButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
     }
     
     @objc private func backButtonTapped() {
@@ -91,11 +114,27 @@ class NavigationBar: UINavigationBar {
     }
     
     func bindTabState() {
-        TabStateManager.shared.currentTabIndex
-            .subscribe(onNext: { [weak self] index in
+        let currentTabIndex = TabStateManager.shared.currentTabIndex
+        
+        // 타이틀 바인딩
+        currentTabIndex
+            .map { index in
                 let titles = TabStateManager.shared.tabTitles
-                self?.navigationTitle.text = index < titles.count ? titles[index] : "홈"
-            })
+                return index < titles.count ? titles[index] : "홈"
+            }
+            .bind(to: navigationTitle.rx.text)
+            .disposed(by: disposeBag)
+        
+        // 홈 탭일 때만 alert 버튼 표시
+        currentTabIndex
+            .map { $0 == 0 }
+            .bind(to: naviAlertBtn.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        // 홈이 아닐 때만 back 버튼 표시
+        currentTabIndex
+            .map { $0 != 0 }
+            .bind(to: naviBackButton.rx.isHidden)
             .disposed(by: disposeBag)
     }
 }
